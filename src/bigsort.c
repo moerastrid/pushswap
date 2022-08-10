9b373279aad8062e695ba8cf23dd6a9ec2460d8e
Astrid Geels <ageels@f1r3s1.codam.nl>
2022-08-10 16:25:23 +0200

    radixz

--- src/bigsort.c
+++ src/bigsort.c
@@ -6,25 +6,59 @@
 /*   By: ageels <ageels@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 21:24:19 by ageels            #+#    #+#             */
-/*   Updated: 2022/07/20 23:07:50 by ageels           ###   ########.fr       */
+/*   Updated: 2022/08/10 16:10:51 by ageels           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pushlib.h"
+#include <stdio.h>
+
+int	ft_find_one(t_info *data)
+{
+	t_list	*temp;
+
+	temp = data->staa;
+	while (temp)
+	{
+		if (temp->con.inr > 0)
+			return (1);
+		temp = temp->nxt;
+	}
+	temp = data->stab;
+	while (temp)
+	{
+		if (temp->con.inr > 0)
+			return (1);
+		temp = temp->nxt;
+	}
+	return (0);
+}
 
 void	ft_bigsort(t_info *data, int fd)
 {
-	int	i;
-	int	half;
+	int		i;
+	t_list	*temp;
 
-	i = 0;
-	half = data->total / 2;
-	while (i < data->total)
+	while (ft_find_one(data) == 1 && ft_issorted(data) == 0)
 	{
-		if (data->staa->con.inr > half)
-			ft_pb(data, fd);
-		else
-			ft_ra(data, fd);
-		i++;
+		i = 0;
+		while (i < data->total)
+		{
+			if (data->staa->con.inr % 2 == 0)
+				ft_pb(data, fd);
+			else
+				ft_ra(data, fd);
+			i++;
+		}
+		while (data->stab)
+			ft_pa(data, fd);
+		temp = data->staa;
+		while (temp)
+		{
+			temp->con.inr = temp->con.inr / 2;
+			temp = temp->nxt;
+		}
 	}
+	while (data->stab)
+		ft_pa(data, fd);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ageels <ageels@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 15:42:01 by ageels            #+#    #+#             */
-/*   Updated: 2022/07/20 21:44:42 by ageels           ###   ########.fr       */
+/*   Updated: 2022/08/10 16:08:11 by ageels           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,9 @@ int	main(int argc, char **argv)
 
 	if (ft_parse(argc, argv, &data) != 0)
 		return (1);
-	ft_putstacks(&data, 1);
 	if (argc <= 6)
 		ft_smolsort(&data, 1);
 	else
 		ft_bigsort(&data, 1);
-	if (ft_issorted(&data) == 1)
-		ft_putstr_fd("\nsorted\n\n", 1);
-	ft_putstacks(&data, 1);
 	return (0);
 }
